5e505a9a251de692c86aeab2f99deb5cccf03b66
David Fischer <davidvfischer@gmail.com>
2024-04-11 12:30:24 +0200

    Refactor Zustand store imports and add adapter functions in Store.ts

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,42 +1,10 @@
-import { create } from 'zustand';
-import { devtools, persist } from 'zustand/middleware';
+import {
+  CartState,
+  CommandState,
+  useCartStoreZustand,
+  useCommandStoreZustand,
+} from './StoreZustand';
 
-import { Command } from '../CommandPattern';
-import { Item } from '../core/Item';
-
-type CommandState = {
-  history: Command[];
-  push: (command: Command) => void;
-  pop: () => Command | undefined;
-};
-
-type CartState = {
-  items: Item[];
-  addItem: (item: Item) => void;
-  removeItem: (item: Item) => void;
-};
-
-export const useCommandStore = create<CommandState>((set, get) => ({
-  history: [],
-  push: (command) => set((state) => ({ history: [...state.history, command] })),
-  // pop the last command from the history
-  pop: () => {
-    const lastCommand = get().history[get().history.length - 1];
-    set((state) => ({ history: state.history.slice(0, -1) }));
-    return lastCommand;
-  },
-}));
-
-export const useCartStore = create<CartState>()(
-  devtools(
-    persist(
-      (set) => ({
-        items: [],
-        addItem: (item) => set((state) => ({ items: [...state.items, item] })),
-        removeItem: (item) =>
-          set((state) => ({ items: state.items.filter((i) => i.id !== item.id) })),
-      }),
-      { name: 'cart-store', getStorage: () => localStorage },
-    ),
-  ),
-);
+// !Adapter to wrap the zustand store in a function (to make the application not depend on zustand)
+export const useCartStore = (): CartState => useCartStoreZustand();
+export const useCommandStore = (): CommandState => useCommandStoreZustand();
